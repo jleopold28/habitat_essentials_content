2457953e2861fe30f6744857cc2155e97720647c
James Leopold <jleopold@shadow-soft.com>
2019-03-26 17:34:48 -0400

    one more time

--- nodejs_demo/habitat/plan.sh
+++ nodejs_demo/habitat/plan.sh
@@ -1,8 +1,9 @@
 pkg_name=nodejs_demo
 pkg_origin=jleopold
-pkg_version="0.3.2"
+pkg_version="0.4.0"
 pkg_scaffolding="core/scaffolding-node"
-
+pkg_svc_user=root
+pkg_svc_group=root
 
 pkg_exports=(
   [port]=app.port
